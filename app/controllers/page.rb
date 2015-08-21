146a67f42f10fceb6f1051e58e3bb2d1dd145177
Marc Bluemner <marc.bluemner@gmail.com>
2015-08-21 09:11:37 +0200

    add customer about, home, contact routes

--- app/controllers/page.rb
+++ app/controllers/page.rb
@@ -1,23 +1,13 @@
 Greenalin::App.controllers :page do
-  
-  # get :index, :map => '/foo/bar' do
-  #   session[:foo] = 'bar'
-  #   render 'index'
-  # end
+	get :about, :map => '/about' do
+		render :erb, 'about'
+	end
 
-  # get :sample, :map => '/sample/url', :provides => [:any, :js] do
-  #   case content_type
-  #     when :js then ...
-  #     else ...
-  # end
+	get :contact, :map => '/contact' do
+		render :erb, 'contact' 
+	end
 
-  # get :foo, :with => :id do
-  #   'Maps to url '/foo/#{params[:id]}''
-  # end
-
-  # get '/example' do
-  #   'Hello world!'
-  # end
-  
-
+	get :home, :map => '/' do
+		render :erb, 'home'
+	end
 end
